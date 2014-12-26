9b54f70b2ee7055b29c953c238afa827bd07a97b
Andrey Baluevsky <a-baluevsky@users.noreply.github.com>
2014-12-26 15:33:28 +0400

    barebone - HelperCore class

--- CUDDocHelper/src/DocHelper.java
+++ CUDDocHelper/src/DocHelper.java
@@ -15,16 +15,29 @@ public class DocHelper {
 	 */
 	public static void main(String[] args) {
 		System.out.println("**** ru.spb.iac.cud Document Helper ***");
-		if( args.length==0) {
-			System.out.println("Usage: DocHelp -cmd");
+		switch(args.length) {
+		case 1:
+			try {
+				DocHelperCore.HelperCommand cmd = 
+						DocHelperCore.HelperCommand.valueOf(args[0]);
+				DocHelperCore h = new DocHelperCore(cmd);
+				System.out.println((h.execute())?"DONE!": "FAILED!");
+				return;
+			} catch(Exception e) {
+				System.out.println(e.getMessage());
+			}
+			//break;	
+		case 0:
+			System.out.println("Usage: DocHelp cmdName");						
+		default:
+			System.out.println("No valid argument specified! Starting interactive mode.");
 			System.out.println("\nPlease select command:");
 			System.out.println("1. Generate CMD files for building projects");
 			System.out.println("2. Generate summary HTML reports for each project");
 			System.out.println("3. Generate project dependency spanning tree");
 			System.out.println("4. Generate package dependency report");
-			System.out.println("q. Press 'q' key to quit");			
-		} else {
-			System.out.println("Not implemented!");
+			System.out.println("q. Press 'q' key to quit");				
+				
 		}		
 	}
 }
